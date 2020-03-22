96bbc93d1b4fbd46a2f7ddd09efd31ced7be88f7
ManuelWaelzlein <gockel98@freenet.de>
2020-03-22 19:08:54 +0100

    geteilt; urls

--- src/Api.tsx
+++ src/Api.tsx
@@ -1,15 +1,15 @@
 const getNumberOfBars = async (): Promise<number> => {
-  const response = await fetch("/numberOfBars");
+  const response = await fetch("http://nodejs-stooped-api-bhbp-8080-app-stooped-api.apps.us-east-1.starter.openshift-online.com/numberOfBars");
   return parseInt(await response.text());
 };
 
 const increment = async (): Promise<number> => {
-  const response = await fetch("/numberOfBars/inc");
+  const response = await fetch("http://nodejs-stooped-api-bhbp-8080-app-stooped-api.apps.us-east-1.starter.openshift-online.com/numberOfBars/inc");
   return parseInt(await response.text());
 };
 
 const decrement = async (): Promise<number> => {
-  const response = await fetch("/numberOfBars/dec");
+  const response = await fetch("http://nodejs-stooped-api-bhbp-8080-app-stooped-api.apps.us-east-1.starter.openshift-online.com/numberOfBars/dec");
   return parseInt(await response.text());
 };
 
