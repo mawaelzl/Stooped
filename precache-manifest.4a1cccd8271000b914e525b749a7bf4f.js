f6e69020e8db6c1ad983cb167c3d4d8bf9647d96
ManuelWaelzlein <gockel98@freenet.de>
2020-03-22 18:20:18 +0100

    Updates

---
+++ precache-manifest.4a1cccd8271000b914e525b749a7bf4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bebacb2fed3270dcc06ce93d95c1c3f",
+    "url": "/Stooped/index.html"
+  },
+  {
+    "revision": "f0198c26337de66a7a37",
+    "url": "/Stooped/static/css/main.7935b4f8.chunk.css"
+  },
+  {
+    "revision": "27ebbab8d35373a1500a",
+    "url": "/Stooped/static/js/2.031e4b68.chunk.js"
+  },
+  {
+    "revision": "233c8d7ff532fc2c0e62392e5ec28675",
+    "url": "/Stooped/static/js/2.031e4b68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0198c26337de66a7a37",
+    "url": "/Stooped/static/js/main.47dfb637.chunk.js"
+  },
+  {
+    "revision": "c98ffe856c2a16761225",
+    "url": "/Stooped/static/js/runtime-main.f723afd5.js"
+  }
+]);
